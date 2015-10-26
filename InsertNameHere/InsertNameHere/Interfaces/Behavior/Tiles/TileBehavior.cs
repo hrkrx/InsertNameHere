10bfa634b3f2556f350cbac76245352831d5cd00
hrkrx <sebastian.leifeld@web.de>
2015-10-26 18:53:41 +0100

    Code sortiert, namespaces müssen noch angepasst werden

---
+++ InsertNameHere/InsertNameHere/Interfaces/Behavior/Tiles/TileBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InsertNameHere.Interfaces.Behavior.Tiles
+{
+    public interface TileBehavior
+    {
+        
+    }
+}
